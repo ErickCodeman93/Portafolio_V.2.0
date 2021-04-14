6b1e6d5e038c49e69867ed02c5249e4316d62924
Erick93Whiskey <erick@whiskeyindustries.net>
2021-04-14 16:26:16 -0500

    deploy heroku

--- models/Server/Server.ts
+++ models/Server/Server.ts
@@ -21,7 +21,7 @@ class Server {
 	constructor(){
 
 		this.app = express();
-		this.port = process.env.PORT || '8000';
+		this.port = process.env.PORT || '8081';
 		this.dirWebPage = process.cwd();
 
 		//Handlebars
